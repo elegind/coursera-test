3414fc3b6edd1c164f54e7908bd64f1a16276022
jimmy <jimmydurando06@gmail.com>
2017-01-07 00:53:59 +0100

    first try of the assignment

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,11 +1,32 @@
 (function(){
+
+  //strict mode so the global namespace is not polluted
   'use strict';
 
-  angular.module("myFirstApp",[])
-  .controller('MyFirstController', function($scope){
-    $scope.name="Jimmy";
-    $scope.sayHello= function(){
-      return "Hello coursera";
+  //declaration of the angular app and controller
+  angular.module("LunchCheck",[])
+  .controller('LunchCheckController', LunchCheckController);
+  
+  //injection of scope service dependency to the Luncheck controller
+  LunchCheckController.$inject = ['$scope'];
+  
+  //javascript function representing the Luncheck controller
+  function LunchCheckController($scope){
+	$scope.list="";
+	$scope.maxItems=3;
+    $scope.stateMessage="";
+	$scope.stateMessageColor="green";
+	
+    $scope.checkIfTooMuch= function(){
+	  var stringList = $scope.list;
+	  var items = stringList.split(',');
+	  if(items && items.length < 4){
+		$scope.stateMessage="Enjoy!";
+		$scope.stateMessageColor="green";
+	  }else{
+		$scope.stateMessage="Too much!";
+		$scope.stateMessageColor="red";
+	  }
     };
-  })
-})();
+  }
+ })();
