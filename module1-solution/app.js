c40b256ed50d7ab1a26eef19680913e12dc85971
jimmy <jimmydurando06@gmail.com>
2017-01-07 19:24:34 +0100

    module 1 solution done

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -6,27 +6,34 @@
   //declaration of the angular app and controller
   angular.module("LunchCheck",[])
   .controller('LunchCheckController', LunchCheckController);
-  
-  //injection of scope service dependency to the Luncheck controller
+
+  //injection of scope service dependency to the Luncheck controller protected from minification
   LunchCheckController.$inject = ['$scope'];
-  
-  //javascript function representing the Luncheck controller
+
+  //function representing the Luncheck controller
   function LunchCheckController($scope){
-	$scope.list="";
-	$scope.maxItems=3;
-    $scope.stateMessage="";
-	$scope.stateMessageColor="green";
-	
+    //variables declaration
+    $scope.list="";
+    $scope.maxItems=3;
+    $scope.messageColor="red";
+
+    //check if there is not too much items in the list
     $scope.checkIfTooMuch= function(){
-	  var stringList = $scope.list;
-	  var items = stringList.split(',');
-	  if(items && items.length < 4){
-		$scope.stateMessage="Enjoy!";
-		$scope.stateMessageColor="green";
-	  }else{
-		$scope.stateMessage="Too much!";
-		$scope.stateMessageColor="red";
-	  }
+      var stringList = $scope.list;
+      var items = stringList.split(',');
+      //if stringList empty then we ask to enter something first
+      if(stringList===""){
+        $scope.message="Please enter data first";
+        $scope.messageColor="red";
+      //if less than 4 items then we display Enjoy!
+      }else if(items.length <= $scope.maxItems){
+        $scope.message="Enjoy!";
+        $scope.messageColor="green";
+      //if more than 3 items then we display Too much!
+      }else{
+        $scope.message="Too much!";
+        $scope.messageColor="red";
+      }
     };
   }
- })();
+})();
