4a786d71db0f83ce6b327fcfc4fdb0cabb56d608
jimmy <jimmydurando06@gmail.com>
2017-01-15 16:08:03 +0100

    assignment done

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -13,29 +13,36 @@
   ToBuyController.$inject = ['ShoppingListCheckOffService'];
   AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 
-  //function representing the ToBuy controller
+  /**
+  *    function representing the ToBuy controller
+  **/
   function ToBuyController(ShoppingListCheckOffService){
     //variables declaration
     this.products = ShoppingListCheckOffService.getProductListToBuy();
 
     this.buyProduct = function(index){
       ShoppingListCheckOffService.buyProduct(index);
-      //this.products = ShoppingListCheckOffService.getProductListToBuy();
     };
 
   }
 
-  //function representing the AlreadyBought controller
+  /**
+  *    function representing the AlreadyBought controller
+  **/
   function AlreadyBoughtController(ShoppingListCheckOffService){
     this.products = ShoppingListCheckOffService.getProductListAlreadyBought();
   }
 
+  /**
+  *    function representing the ShoppingListCheckOffService
+  **/
   function ShoppingListCheckOffService(){
     var shoppingService = this;
     //business variable shared between the controllers, arrays of products to buy or already bought
     var productListToBuy = [{ name: "cookies", quantity: 10 }, { name: "eggs", quantity: 12 }, { name: "water bottles", quantity: 4 }, { name: "fishes", quantity: 2 } , { name: "soda can", quantity: 1 }];
     var productListAlreadyBought = [];
 
+    //business function of the service
     shoppingService.getProductListToBuy = function(){
       return productListToBuy;
     };
@@ -45,8 +52,7 @@
     };
 
     shoppingService.buyProduct = function(index){
-      productListAlreadyBought.push(productListToBuy[index]);
-      productListToBuy.slice(index,1);
+      productListAlreadyBought.push(productListToBuy.splice(index,1)[0]);
     };
 
   }
